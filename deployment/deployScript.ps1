260e522f426ec3cd75b363b1d882b763e7a8c9c2
rabee333 <rabee333@gmail.com>
2017-09-14 12:31:42 +0300

    Make some fixes to the deployment script

--- deployment/deployScript.ps1
+++ deployment/deployScript.ps1
@@ -4,7 +4,7 @@ $etheriumConsertiumTemplateFilePath = "etheriumConsertiumTemplate.json";
 $etheriumConsertiumParametersFilePath = "etheriumConsertiumParameters.json";
 $supplyChainTemplateFilePath = "supplyChainTemplate.json";
 $supplyChainParametersFilePath = "supplyChainParameters.json";
-$documentServicesEndPoint = 'https://ibera-document-service.azurewebsites.net/api/Attachment';
+$documentServicesEndPoint = 'https://ibera-document-service.azurewebsites.net';
 
 #Read the configuration file
 Get-Content $confFileName | foreach-object -begin {$conf=@{}} -process { $k = [regex]::split($_,'='); if(($k[0].CompareTo("") -ne 0) -and ($k[0].StartsWith("[") -ne $True)) { $conf.Add($k[0], $k[1]) } }
@@ -83,11 +83,51 @@ $etheriumConsertiumParameters | ConvertTo-Json  | set-content $etheriumConsertiu
 Write-Host "Starting Etherium deployment...";
 $etheriumConsertiumOutput = New-AzureRmResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateFile $etheriumConsertiumTemplateFilePath -TemplateParameterFile $etheriumConsertiumParametersFilePath;
 
-$ethTxVmRpcEndpoint = $etheriumConsertiumOutput.Outputs.'ethereum-rpc-endpoint'.value;
+$ethRpcEndpoint = $etheriumConsertiumOutput.Outputs.'ethereum-rpc-endpoint'.value;
 $ethVnetName = (Get-AzureRmVirtualNetwork -ResourceGroupName $resourceGroupName).name;
 
-Write-Host "ethTxVmRpcEndpoint = '$ethTxVmRpcEndpoint'";
+Write-Host "ethRpcEndpoint = '$ethRpcEndpoint'";
 Write-Host "ethVnetName = '$ethVnetName'";
+
+#Clone Smart Contracts repo and install it:
+$supplyChainPath = pwd;
+cd ..
+git clone $conf["gitSmartContractsRepoURL"]
+cd(Get-ChildItem -Directory . | sort CreationTime | select -l 1);
+npm install
+
+#Create public IP for the Etherium TX0 VM:
+$pip = New-AzureRmPublicIpAddress -Name "tx0PublicIpAddress" -ResourceGroupName $resourceGroupName -AllocationMethod "Static" -Location $resourceGroupLocation;
+$txNic = Get-AzureRmNetworkInterface -Name "nic-tx0" -ResourceGroupName $resourceGroupName;
+$txNic.IpConfigurations[0].PublicIpAddress = $pip;
+Set-AzureRmNetworkInterface -NetworkInterface $txNic;
+Write-Host "Created a new public IP address to the Ethereum TX0 VM: $pip.IpAddress";
+$ethRpcTx0Endpoint = "http://" + $pip.IpAddress + ":8545";
+
+#Deploy the smart contract to the Etherium TX VM:
+$deployCommand = "node deploy ProofOfProduceQuality $ethRpcTx0Endpoint $etheriumTxRpcPassword"
+Write-Host "Deploying smart contracts using the following command: $deployResult";
+$deployResult = Invoke-Expression "$deployCommand 2>&1" 
+Write-Host $deployResult
+
+#Removing the public IP for the Etherium TX0 VM:
+#$txNic.IpConfigurations[0].PublicIpAddress = "";
+
+#Get the deployment result and extract the Account and Contract addresses in case of success or the Error string in case of failure:
+$deployResultJson = ('{'+($deployResult -split '{')[-1] | ConvertFrom-Json)
+$deploymentError = $deployResultJson.error;
+if($deploymentError){
+	Write-Host 'deploymentError= ' $deploymentError;
+}else{
+	$accountAddress = $deployResultJson.accountAddress;
+	$contractAddress = $deployResultJson.contractAddress;
+	Write-Host 'accountAddress= ' $accountAddress;
+	Write-Host 'contractAddress= ' $contractAddress;
+}
+
+#Return to the original Supplu Chain project directory
+cd $supplyChainPath
+
 
 $supplyChainParameters = Get-Content $supplyChainParametersFilePath -raw | ConvertFrom-Json;
 
@@ -129,35 +169,6 @@ $storageKey = (Get-AzureRmStorageAccountKey -ResourceGroupName $resourceGroupNam
 $storageConnectionString = 'DefaultEndpointsProtocol=https;AccountName=' + $storageName + ';AccountKey=' + $storageKey + ';EndpointSuffix=core.windows.net';
 Write-Host "storageConnectionString = '$storageConnectionString'";
 
-
-#Clone Smart Contracts repo and install it:
-$supplyChainPath = pwd;
-cd ..
-git clone $conf["gitSmartContractsRepoURL"]
-cd(Get-ChildItem -Directory . | sort CreationTime | select -l 1);
-npm install
-
-#Deploy the smart contract to the Etherium TX VM:
-$deployCommand = "node deploy ProofOfProduceQuality $ethTxVmRpcEndpoint $etheriumTxRpcPassword"
-$deployResult = Invoke-Expression "$deployCommand 2>&1" 
-Write-Host $deployResult
-
-#Get the deployment result and extract the Account and Contract addresses in case of success or the Error string in case of failure:
-$deployResultJson = ('{'+($deployResult -split '{')[-1] | ConvertFrom-Json)
-$deploymentError = $deployResultJson.error;
-if($deploymentError){
-	Write-Host 'deploymentError= ' $deploymentError;
-}else{
-	$accountAddress = $deployResultJson.accountAddress;
-	$contractAddress = $deployResultJson.contractAddress;
-	Write-Host 'accountAddress= ' $accountAddress;
-	Write-Host 'contractAddress= ' $contractAddress;
-}
-
-#Return to the original Supplu Chain project directory
-cd $supplyChainPath
-
-
 #Setting Services webapp environment variables:
 $servicesWebapp = Get-AzureRMWebAppSlot -ResourceGroupName $resourceGroupName -Name $servicesWebappName -Slot production
 $servicesWebappSettingList = $servicesWebapp.SiteConfig.AppSettings
@@ -170,7 +181,7 @@ $servicesWebappHash['CONTRACT_ADDRESS'] = $contractAddress;
 $servicesWebappHash['ACCOUNT_ADDRESS'] = $accountAddress;
 $servicesWebappHash['ACCOUNT_PASSWORD'] = $etheriumTxRpcPassword;
 $servicesWebappHash['GAS'] = '2000';
-$servicesWebappHash['GET_RPC_ENDPOINT'] = $ethTxVmRpcEndpoint;
+$servicesWebappHash['GET_RPC_ENDPOINT'] = $ethRpcEndpoint;
 $servicesWebappHash['AZURE_STORAGE_CONNECTION_STRING'] = $storageConnectionString;
 
 Set-AzureRMWebAppSlot -ResourceGroupName $resourceGroupName -Name $servicesWebappName -AppSettings $servicesWebappHash -Slot production
